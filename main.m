fa5fc70beb697f1e4644895fc03e0a58f97beb49
prs513rosewood <lucas.frerot@gmail.com>
2010-06-12 16:09:32 +0200

    Reads music given by argument.

--- main.m
+++ main.m
@@ -3,16 +3,27 @@
 
 #include <stdlib.h>
 
+#define PLAY(sound) [sound play];\
+		sleep ( [sound duration] );\
+		[sound stop];
+
 int main (int argc, char * argv[])
 {
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	
-	NSSound * sound = [[NSSound alloc] initWithContentsOfFile:@"sound.mp3" byReference:NO];
-	[sound play];
-	sleep ( [sound duration] );
-	[sound stop];
-	[sound release];
+	NSArray * args = [[NSProcessInfo processInfo] arguments];
 
+	if ( [args count] == 1) {
+		exit (-1);
+	}
+
+	NSData * soundSource = [NSData dataWithContentsOfFile:[args objectAtIndex:1]];
+
+	if (!soundSource)
+		exit(-1);
+
+	NSSound * music = [[NSSound alloc] initWithData:soundSource];
+	PLAY(music);
+	[music release];
 	[pool drain];
 	return 0;
 }
