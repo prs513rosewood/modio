3516e2c78261b42dcc081b7bd9a94f01f791de20
prs513rosewood <lucas.frerot@gmail.com>
2010-06-12 23:11:06 +0200

    Init method now searches for the songs in the playlist.

--- Playlist.h
+++ Playlist.h
@@ -1,12 +1,16 @@
 #import <Foundation/Foundation.h>
+#import "constants.h"
 
 @interface Playlist : NSObject
 {
-	NSArray * songs;
+	NSMutableArray * songs;
+	NSMutableArray * lines;
 	int mode;
 }
 
 - (id) initWithFile:(NSString *) fileName;
+- (id) songs;
+- (void) dealloc;
 
 @end
 
--- Playlist.m
+++ Playlist.m
@@ -5,10 +5,55 @@
 - (id) initWithFile:(NSString *) fileName
 {
 	if (self = [super init]) {
-		
+		lines = [[NSMutableArray alloc] init];
+		songs = [[NSMutableArray alloc] init];
+		NSString * contents = [NSString stringWithContentsOfFile:fileName];
+		NSString * aLine = nil, prefix = nil;
+		NSRange range = NSMakeRange(0, 0);
+		int i;
+
+		for (i = 0 ; i < [contents length] ; i++) {
+			if ([contents caracterAtIndex:i] == '\n') {
+				range.length = i - range.location;
+				[lines addObject:[contents substringWithRange:range]];
+				range.location = i + 1;
+			}
+		}
+
+		for (aLine in lines) {
+			switch ([aLine characterAtIndex:0]) {
+				case ':':
+					prefix = [NSString stringWithstring:[aLine substringFromIndex:1]];
+					break;
+				case '!':
+					if (([aLine rangeOfString:@"rand"]).location != NSNotFound)
+						mode |= RAND;
+					if (([aLine rangeOfString:@"loop"]).location != NSNotFound)
+						mode |= LOOP;
+					break;
+				case '>':
+					[songs addObject:[prefix stringByAppndingString:[aLine substringFromIndex:1]];
+					break;
+				default:
+					break;
+			}
+		}
+
 	}
 
 	return self;
+}
+
+- (id) songs
+{
+	return [[songs retain] autorelease];
+}
+
+- (void) dealloc
+{
+	[lines release];
+	[songs release];
+	[super dealloc];
 }
 
 @end
