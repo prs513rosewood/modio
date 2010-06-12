598453b6269059380f8223a5b1f1177e634730f6
prs513rosewood <lucas.frerot@gmail.com>
2010-06-12 21:26:04 +0200

    Modio 0.1 version : reads music files from arguments.

--- main.m
+++ main.m
@@ -11,19 +11,37 @@ int main (int argc, char * argv[])
 {
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	NSArray * args = [[NSProcessInfo processInfo] arguments];
+	NSData * soundSource = nil;
+	NSString * fileName;
+	NSSound * music = nil;
+	NSEnumerator * enumerator = nil;
 
-	if ( [args count] == 1) {
-		exit (-1);
+	if (argc < 2) {
+		printf ("usage: modio filename ...\n");
+		return -1;
 	}
 
-	NSData * soundSource = [NSData dataWithContentsOfFile:[args objectAtIndex:1]];
+	enumerator = [args objectEnumerator];
+	[enumerator nextObject];
 
-	if (!soundSource)
-		exit(-1);
+	while (fileName = [enumerator nextObject]) {
+		if (!(soundSource = [NSData dataWithContentsOfFile:fileName])) {
+			printf ("file not found: %s\n", [fileName UTF8String]);
+		}
 
-	NSSound * music = [[NSSound alloc] initWithData:soundSource];
-	PLAY(music);
-	[music release];
+		else {
+			music = [[NSSound alloc] initWithData:soundSource];
+			if (!music) {
+				printf ("could not read file: %s\n", [fileName UTF8String]);
+			}
+
+			else {
+				PLAY(music);
+				[music release];
+				music = nil;
+			}
+		}
+	}
 	[pool drain];
 	return 0;
 }
