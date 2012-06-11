95559bc81f540073d7809117e63a9aa734332473
prs513rosewood <lucas.frerot@gmail.com>
2012-06-11 13:50:30 +0200

    Removing trailing space characters for paths in playlists and removed ppc architecture

--- src/Playlist.m
+++ src/Playlist.m
@@ -60,7 +60,7 @@ Copyright (C) 2010  Lucas Frérot
 						}
 
 						else {
-							songPath = [[aLine substringFromIndex:1] stringByStandardizingPath];
+							songPath = [[[aLine substringFromIndex:1] stringByStandardizingPath] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
 							if ( [songPath isAbsolutePath] || !prefix)
 								[songs addObject:songPath];
 							else
